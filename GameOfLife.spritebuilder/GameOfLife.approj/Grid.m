ce0ffc280d2f678e7e23b6f1230ed4c69fdaa6f3
ztylff7 <Legend1883@Tianyis-MacBook-Pro.local>
2014-07-07 08:33:24 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/GameOfLife.approj/Grid.m
+++ GameOfLife.spritebuilder/GameOfLife.approj/Grid.m
@@ -56,13 +56,33 @@ static const int GRID_COLUMNS = 10;
             // this is shorthand to access an array inside an array
             _gridArray[i][j] = creature;
             
-            // make creatures visible to test this method, remove this once we know we have filled the grid properly
-            creature.isAlive = YES;
-            
             x+=_cellWidth;
         }
         
         y += _cellHeight;
     }
 }
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    //get the x,y coordinates of the touch
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    //get the Creature at that location
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    
+    //invert it's state - kill it if it's alive, bring it to life if it's dead.
+    creature.isAlive = !creature.isAlive;
+}
+
+- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
+{
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    int row;
+    int column;
+    row = touchPosition.y /_cellHeight;
+    column = touchPosition.x / _cellWidth;
+    return _gridArray[row][column];
+}
+
 @end
