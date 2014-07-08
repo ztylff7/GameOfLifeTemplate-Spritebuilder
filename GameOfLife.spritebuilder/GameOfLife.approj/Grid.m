74e0eec3d0db5fc3b7f6538e33c0c09c5171e232
ztylff7 <Legend1883@Tianyis-MacBook-Pro.local>
2014-07-07 22:55:01 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/GameOfLife.approj/Grid.m
+++ GameOfLife.spritebuilder/GameOfLife.approj/Grid.m
@@ -162,6 +162,32 @@ static const int GRID_COLUMNS = 10;
                 // access the creature in the cell that corresponds to the current row/column
                 Creature *currentCreature = _gridArray[i][j];
                 
+                // remember that every creature has a 'livingNeighbors' property that we created earlier
+                currentCreature.livingNeighbors = 0;
+                
+                // now examine every cell around the current one
+                
+                // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
+                for (int x = (i-1); x <= (i+1); x++)
+                {
+                    // go through the column to the left of the current cell, the column the cell is in, and the column to the right of the current cell
+                    for (int y = (j-1); y <= (j+1); y++)
+                    {
+                        // check that the cell we're checking isn't off the screen
+                        BOOL isIndexValid;
+                        isIndexValid = [self isIndexValidForX:x andY:y];
+                        
+                        // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
+                        if (!((x == i) && (y == j)) && isIndexValid)
+                        {
+                            Creature *neighbor = _gridArray[x][y];
+                            if (neighbor.isAlive)
+                            {
+                                currentCreature.livingNeighbors += 1;
+                            }
+                        }
+                    }
+                }
                 if (currentCreature.livingNeighbors == 3)
                 {currentCreature.isAlive = TRUE;
                 }
@@ -171,7 +197,7 @@ static const int GRID_COLUMNS = 10;
                 else if (currentCreature.livingNeighbors >=4)
                 {currentCreature.isAlive = FALSE;
                 }
-                _gridArray[i][j] = currentCreature;
+                _gridArray[i][j]=currentCreature;
             }
         }
     }
